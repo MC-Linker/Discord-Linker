fba0c8ca4b63bd3765bba384d866f60b555a5632
Lianecx <84291452+Lianecx@users.noreply.github.com>
2022-08-21 21:57:04 +0200

    Added command permissions

--- src/main/java/me/lianecx/discordlinker/LinkerCommand.java
+++ src/main/java/me/lianecx/discordlinker/LinkerCommand.java
@@ -4,7 +4,6 @@ import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
 
 import java.util.Arrays;
 
@@ -14,8 +13,8 @@ public class LinkerCommand implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if(args.length == 0) return false;
-        if(sender instanceof Player && !sender.isOp()) {
-            sender.sendMessage(ChatColor.RED + "This command can only be executed by admins.");
+        if(!sender.hasPermission("discordlinker.linker")) {
+            sender.sendMessage(ChatColor.RED + "You do not have permission to use this command.");
             return true;
         }
 
