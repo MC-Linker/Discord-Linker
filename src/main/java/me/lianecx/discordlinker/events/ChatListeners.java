d33c445725c7c8d791aae3ac170659e4ca67c40f
Lianecx <84291452+Lianecx@users.noreply.github.com>
2023-06-27 17:42:09 +0200

    Changed chat event priorities to MONITOR

--- src/main/java/me/lianecx/discordlinker/events/ChatListeners.java
+++ src/main/java/me/lianecx/discordlinker/events/ChatListeners.java
@@ -14,43 +14,43 @@ import org.bukkit.event.server.ServerCommandEvent;
 
 public class ChatListeners implements Listener {
 
-    @EventHandler(ignoreCancelled = true, priority = EventPriority.HIGH)
+    @EventHandler(ignoreCancelled = true, priority = EventPriority.MONITOR)
     public void onChatMessage(AsyncPlayerChatEvent event) {
         //Remove color codes
         String replacedMessage = ChatColor.stripColor(event.getMessage().replaceAll("(?i)&[0-9A-FK-OR]", ""));
         sendChatAsync(replacedMessage, ChatType.CHAT, event.getPlayer().getName());
     }
 
-    @EventHandler(ignoreCancelled = true, priority = EventPriority.HIGH)
+    @EventHandler(ignoreCancelled = true, priority = EventPriority.MONITOR)
     public void onPlayerJoin(PlayerJoinEvent event) {
         sendChatAsync(event.getJoinMessage(), ChatType.JOIN, event.getPlayer().getName());
         sendStatsAsync(StatsUpdateEvent.MEMBERS);
     }
 
-    @EventHandler(ignoreCancelled = true, priority = EventPriority.HIGH)
+    @EventHandler(ignoreCancelled = true, priority = EventPriority.MONITOR)
     public void onPlayerQuit(PlayerQuitEvent event) {
         sendChatAsync(event.getQuitMessage(), ChatType.QUIT, event.getPlayer().getName());
         sendStatsAsync(StatsUpdateEvent.MEMBERS);
     }
 
-    @EventHandler(ignoreCancelled = true, priority = EventPriority.HIGH)
+    @EventHandler(ignoreCancelled = true, priority = EventPriority.MONITOR)
     public void onAdvancement(PlayerAdvancementDoneEvent event) {
         //Dont process recipes
         if(event.getAdvancement().getKey().toString().startsWith("minecraft:recipes/")) return;
         sendChatAsync(event.getAdvancement().getKey().toString(), ChatType.ADVANCEMENT, event.getPlayer().getName());
     }
 
-    @EventHandler(ignoreCancelled = true, priority = EventPriority.HIGH)
+    @EventHandler(ignoreCancelled = true, priority = EventPriority.MONITOR)
     public void onPlayerDeath(PlayerDeathEvent event) {
         sendChatAsync(event.getDeathMessage(), ChatType.DEATH, event.getEntity().getName());
     }
 
-    @EventHandler(ignoreCancelled = true, priority = EventPriority.HIGH)
+    @EventHandler(ignoreCancelled = true, priority = EventPriority.MONITOR)
     public void onPlayerCommand(PlayerCommandPreprocessEvent event) {
         sendChatAsync(event.getMessage(), ChatType.PLAYER_COMMAND, event.getPlayer().getName());
     }
 
-    @EventHandler(ignoreCancelled = true, priority = EventPriority.HIGH)
+    @EventHandler(ignoreCancelled = true, priority = EventPriority.MONITOR)
     public void onConsoleCommand(ServerCommandEvent event) {
         //Command sender either from the console or a command block
         ChatType commandType = event.getSender() instanceof ConsoleCommandSender ? ChatType.CONSOLE_COMMAND : ChatType.BLOCK_COMMAND;
