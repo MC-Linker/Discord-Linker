081b0ede53f00f67efc52fc20cd1a45a811efc27
Lianecx <emilian.klefe@web.de>
2025-07-16 16:57:11 +0200

    Deprecated `connect` method in HttpAdapter
    Signed-off-by: Lianecx <emilian.klefe@web.de>

--- src/main/java/me/lianecx/discordlinker/network/adapters/HttpAdapter.java
+++ src/main/java/me/lianecx/discordlinker/network/adapters/HttpAdapter.java
@@ -61,11 +61,12 @@ public class HttpAdapter implements NetworkAdapter {
         });
     }
 
+    @Deprecated
     public void connect(int port, Consumer<Boolean> callback) {
-        app.listen(() -> {
-            callback.accept(true);
-            DiscordLinker.getPlugin().getLogger().info("Listening on port " + port);
-        }, port);
+//        app.listen(() -> {
+//            callback.accept(true);
+//            DiscordLinker.getPlugin().getLogger().info("Listening on port " + port);
+//        }, port);
     }
 
     public static Router.RouterResponse send(RequestMethod method, String route, JsonElement body) {
